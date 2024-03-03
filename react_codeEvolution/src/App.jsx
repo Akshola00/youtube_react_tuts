1638ef8fa57bbf269e4131ee456784125e172e55
Akshola00 <akinniyishola07@gmail.com>
2024-03-03 23:32:38 +0100

    progress

--- react_codeEvolution/src/App.jsx
+++ react_codeEvolution/src/App.jsx
@@ -4,6 +4,8 @@ import Counter from './components/Counter'
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import './App.css'
+import './appStyles.css'
+import styles from './appStyles.module.css'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
 import EventBing from './components/EventBing'
@@ -11,13 +13,17 @@ import ParentComponent from './components/ParentComponent'
 import UserGreeting from './components/UserGreeting'
 import NalmeList from './components/NalmeList'
 import Stylesheet from './components/Stylesheet'
+import Inline from './components/Inline'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Stylesheet />
+      <h2 className='error'>error</h2>
+      <h3 className={styles.success}>success</h3>
+      {/* <Stylesheet primary={true} /> */}
+      <Inline />
       {/* <Message />
       <Counter />
       <Greet name="John Cena" heroname="UNOCME" /> */}
--- react_codeEvolution/src/components/Stylesheet.jsx
+++ react_codeEvolution/src/components/Stylesheet.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Mystyles.css'
-function Stylesheet() {
+function Stylesheet(props) {
+  let className = props.primary ? 'primary' : ''
   return (
     <div>
-      <h1 className='primary' >Stylesheets</h1>
+      <h1 className={`${className} font-xl`} >Stylesheets</h1>
     </div>
   )
 }
