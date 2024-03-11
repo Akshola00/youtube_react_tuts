5981f50787563942514dddb27209d74213b5e494
Akshola00 <akinniyishola07@gmail.com>
2024-03-11 12:13:55 +0100

    lorem

--- react_codeEvolution/src/components/Form.jsx
+++ react_codeEvolution/src/components/Form.jsx
@@ -1,29 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Form extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         username : ''
-      }
+  constructor(props) {
+    super(props);
 
-    }
-    handleUsernameChange = (event) => {
-        this.setState({
-            username: event.target.value
-        })
-      }
+    this.state = {
+      username: "",
+      comments: "",
+      topic: "react",
+    };
+  }
+  handleUsernameChange = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+  handlecommentschnage = (event) => {
+    this.setState({
+      comments: event.target.value,
+    });
+  };
+  handleTopicChange = (event) => {
+    this.setState({
+      topic: event.target.value,
+    });
+  };
+  handlesubmit = (event) => {
+    alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+    event.preventDefault()
+  }
   render() {
     return (
-      <form action="">
+      <form onSubmit={this.handlesubmit} action="">
         <div>
-            <label htmlFor="username">username</label>
-            <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
         </div>
+        <div>
+          <label htmlFor="">comments</label>
+          <textarea
+            onChange={this.handlecommentschnage}
+            value={this.state.comments}
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+        <div>
+          <label htmlFor="">topic</label>
+          <select
+            name=""
+            id=""
+            value={this.state.topic}
+            onChange={this.handleTopicChange}
+          >
+            <option value="react">react</option>
+            <option value="angular">angular</option>
+            <option value="view">view</option>
+          </select>
+        </div>
+        <button type="submit">submit</button>
       </form>
-    )
+    );
   }
 }
 
-export default Form
+export default Form;
