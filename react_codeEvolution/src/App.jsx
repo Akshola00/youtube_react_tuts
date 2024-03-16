c19d8b21c08fc7161345305f371930f2d2484cbc
Akshola00 <akinniyishola07@gmail.com>
2024-03-16 19:29:00 +0100

    progress

--- react_codeEvolution/src/App.jsx
+++ react_codeEvolution/src/App.jsx
@@ -1,31 +1,34 @@
-import { Fragment, PureComponent, useState } from 'react'
-import Message from './components/message'
-import Counter from './components/Counter'
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import './App.css'
-import './appStyles.css'
-import styles from './appStyles.module.css'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBing from './components/EventBing'
-import ParentComponent from './components/ParentComponent'
-import UserGreeting from './components/UserGreeting'
-import NalmeList from './components/NalmeList'
-import Stylesheet from './components/Stylesheet'
-import Inline from './components/Inline'
-import Form from './components/Form'
-import LifecycleA from './components/LifecycleA'
-import Fragmentdemo from './components/Fragmentdemo'
-import Table from './components/Table'
-import Regularcomponent from './components/Regularcomponent'
-import PureandRegComp from './components/PureandRegComp'
-import RefsDemo from './components/RefsDemo'
-import Focusinput from './components/Focusinput'
-import FRParentinput from './components/FRParentinput'
+import { Fragment, PureComponent, useState } from "react";
+import Message from "./components/message";
+import Counter from "./components/Counter";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import "./App.css";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBing from "./components/EventBing";
+import ParentComponent from "./components/ParentComponent";
+import UserGreeting from "./components/UserGreeting";
+import NalmeList from "./components/NalmeList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import Form from "./components/Form";
+import LifecycleA from "./components/LifecycleA";
+import Fragmentdemo from "./components/Fragmentdemo";
+import Table from "./components/Table";
+import Regularcomponent from "./components/Regularcomponent";
+import PureandRegComp from "./components/PureandRegComp";
+import RefsDemo from "./components/RefsDemo";
+import Focusinput from "./components/Focusinput";
+import FRParentinput from "./components/FRParentinput";
+import PortalDemo from "./components/PortalDemo";
+import Hero from "./components/Hero";
+import Errorboundary from "./components/Errorboundary";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
@@ -36,7 +39,18 @@ function App() {
       {/* <PureandRegComp /> */}
       {/* <RefsDemo/> */}
       {/* <Focusinput/> */}
-      <FRParentinput />
+      {/* <FRParentinput /> */}
+      {/* <PortalDemo /> */}
+      <Errorboundary>
+        <Hero heroname={"batman"} />
+      </Errorboundary>
+      <Errorboundary>
+        <Hero heroname={"superman"} />
+      </Errorboundary>
+      <Errorboundary>
+        <Hero heroname={"joker"} />
+      </Errorboundary>
+
       {/* <h2 className='error'>error</h2>
       <h3 className={styles.success}>success</h3> */}
       {/* <Stylesheet primary={true} /> */}
@@ -52,7 +66,7 @@ function App() {
       {/* <UserGreeting /> */}
       {/* < NalmeList /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
